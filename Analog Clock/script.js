9b3211cfb9fe44f89b8e5d8c65ddf4348d2a5b3b
Achmad Fauzan Alif Fitrah <fznfitrah@gmail.com>
2025-07-14 12:07:02 +0700

    build: make mini-project analog clock

---
+++ Analog Clock/script.js
@@ -0,0 +1,20 @@
+const hours = document.querySelector(".hour-hand");
+const minutes = document.querySelector('.minute-hand');
+const seconds = document.querySelector('.second-hand');
+
+const myClock = setInterval ( () => {
+    let now = new Date();
+    let second = now.getSeconds();
+    let minute = now.getMinutes();
+    let hour = now.getHours();
+    
+    seconds.style.transform =  `translateY(-100%) rotate(${second*6}deg)`;
+    minutes.style.transform =  `translateY(-100%) rotate(${minute*6}deg)`;
+    hours.style.transform =  `translateY(-100%) rotate(${hour*6}deg)`;
+
+}, 1000);
+
+// const myMinute = setInterval ( () => {
+//     let now = new Date();
+
+// }, 1000);
