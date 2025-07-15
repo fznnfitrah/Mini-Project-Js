21c48d069a9fa6cff5ec11e39cce624015029197
Achmad Fauzan Alif Fitrah <fznfitrah@gmail.com>
2025-07-15 07:34:55 +0700

    build: analog clock done

--- Analog Clock/script.js
+++ Analog Clock/script.js
@@ -7,9 +7,17 @@ const myClock = setInterval ( () => {
     let second = now.getSeconds();
     let minute = now.getMinutes();
     let hour = now.getHours();
-    
-    seconds.style.transform =  `translateY(-100%) rotate(${second*6}deg)`;
-    minutes.style.transform =  `translateY(-100%) rotate(${minute*6}deg)`;
-    hours.style.transform =  `translateY(-100%) rotate(${hour*6}deg)`;
+
+    let secondDegree = second * 6
+    seconds.style.transform =  `translateY(-100%) rotate(${secondDegree}deg)`;
+
+    let minuteDegree = (minute * 6) + (second * 0.1)
+    minutes.style.transform =  `translateY(-100%) rotate(${minuteDegree}deg)`;
+
+
+    let adjustHour = hour % 12;
+    let hourDegree = (adjustHour * 30) + (minute * 0.5) + (second * (0.5/60));
+    hours.style.transform =  `translateY(-100%) rotate(${hourDegree}deg)`;
+
 
 }, 1000);
